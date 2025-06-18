61f42768507069c0c93499eb0e23af1b049f0bf7
nikhil <nikhil@entryzones.com>
2025-06-18 14:24:38 +0530

    app

--- src/App.js
+++ src/App.js
@@ -13,23 +13,6 @@ import AssignmentPage from './pages/AssignmentPage';
 import ClassPage from './components/QuizzSection';
 import HomePage from './pages/HomePage';
 import PerfectCourseSection from './components/PerfectCourseSection';
-<<<<<<< HEAD
-import PopularSubjects from './components/PopularSubjects';
-import DashboardPage from './components/admin/DashboardPage';
-import Dashboard from './components/admin/Dashboard';
-import DashboardSection from './components/admin/DashboardSection';
-import NoticeBoardAndNotifications from './components/admin/NoticeBoardAndNotifications';
-import TotalFeedbacks from './components/admin/totalfeedback';
-import EdurockFooter from './components/admin/EdurockFooter';
-import UserProfile from './components/admin/UserProfile';
-import ChatDashboard from './components/admin/ChatUI';
-import CourseCard from './components/CourseCard';
-import CourseDashboard from './components/admin/courses';
-import QuizAttemptsPage from './components/admin/quizData';
-import UserProfilePage from './components/admin/UserProfilePage';
-import CreateCourse from './components/admin/CreateCourse';
-
-=======
 import StartQuizPage from './pages/StartQuizPage';
 import ContactUs from './components/ContactUs';
 import Appbar from './components/Appbar';
@@ -38,7 +21,6 @@ import PaymentPage from './pages/PaymentPage';
 import SuccessPage from './pages/SuccessPage';
 import QuizQuestions from './components/QuizQuestions';
 import QuizResult from './components/QuizResult';
->>>>>>> 2ba23e03b2e478ecbe87a38cfa9951c9248e3fcf
 
 
 function App() {
@@ -51,25 +33,9 @@ function App() {
   
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Navbar/>
-      <DashboardPage/>
-       <Dashboard/>
-      <DashboardSection/>
-      <NoticeBoardAndNotifications/>
-      <TotalFeedbacks/>
-      <EdurockFooter/>
-      <UserProfile/>
-      <ChatDashboard/>
-      <CourseDashboard/>
-      <QuizAttemptsPage/>
-      <UserProfilePage/>
-      <CreateCourse/>
-=======
      {/* {showNavbar && <Navbar/>}  */}
          {!hideNavbar && <Navbar />}
       {/* {!hideNavbarRoutes.includes(location.pathname) && <Appbar />} */}
->>>>>>> 2ba23e03b2e478ecbe87a38cfa9951c9248e3fcf
         <Routes>
            <Route path="/" element={<HomePage />} />
         {/* <Route path="/" element={<Landingpage />} /> */}
